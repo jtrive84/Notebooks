ba9ba8ca17ee45a8fba3d58069f487c69f453d6e
jtrive84 <james.triveri@gmail.com>
2017-10-09 16:39:07 -0500

    Added section on map.

--- applyFamily/makeSlides.sh
+++ applyFamily/makeSlides.sh
@@ -2,15 +2,15 @@
 
 JUPYTER='/cygdrive/c/Anaconda3/Scripts/jupyter.exe'
 
-pushd '/cygdrive/e/Repos/Notebooks/applyFamily'
+pushd '/cygdrive/u/Repos/Notebooks/applyFamily'
 
 # check if file exists, if so, delete it =>
-if [ -e applyFamily-Test.ipynb ]
-then
-    rm -f applyFamilt-Test.ipynb
-else
-    echo 'applyFamilt-Test.ipynb not present...'
-fi
+#if [ -e applyFamily-Test.ipynb ]
+#then
+#    rm -f applyFamilt-Test.ipynb
+#else
+#    echo 'applyFamilt-Test.ipynb not present...'
+#fi
 
 # check if html file exists, if so, delete it =>
 if [ -e applyFamily-Test.slides.html ]
@@ -21,12 +21,12 @@ else
 fi
 
 # copy notebook from ghpages repository =>
-cp '/cygdrive/e/Repos/ghpages/Notebooks/applyFamily-Test.ipynb' .
+#cp '/cygdrive/e/Repos/ghpages/Notebooks/applyFamily-Test.ipynb' .
 
 # generate slides =>
 # $JUPYTER nbconvert applyFamily-Test.ipynb --to slides --post serve
 
 $JUPYTER nbconvert applyFamily-Test.ipynb --to slides --reveal-prefix reveal.js-master
 
-/cygdrive/c/Anaconda3/python.exe -m http.server 8001 --bind 127.0.0.1
+/cygdrive/c/Anaconda3/python.exe -m http.server 8002 --bind 127.0.0.1
 
